74e5a5a756bc219bee9068ca03c03986d5be39c1
Yasshiemasa <masa.19851012@icloud.com>
2020-05-10 22:30:12 +0900

    implement reducer

---
+++ src/action/indwx.js
@@ -0,0 +1,13 @@
+const INCREMENT = "INCREMENT" 
+
+export const increment = () => ({
+    return {
+
+        type: 'INCREMENT'
+    })
+
+export const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }      
+}
