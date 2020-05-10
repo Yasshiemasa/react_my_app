18d41863c1db555ceaeb315752774beb83fb4c2c
Yasshiemasa <masa.19851012@icloud.com>
2020-05-10 17:42:04 +0900

    learn props of componemt

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 //必須
 import React from 'react';
 
-// //Appクラス = Appコンポーネント(カスタムタグ)
-// class App extends Component {
-//   render() {
-//    return (
-//    <React.Fragment>
-//      <label htmlFor="bar">bar</label>
-//      <input type="text" onChange={() => {console.log("I am clicked")}} />
-//    </React.Fragment>
-//    )
-//   }
-// }
-
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Noname" }
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      } 
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+return <div>Hi! I am {props.name}, and {props.age} years old!</div>
 }
 
-
+User.defaultProps = {
+  age: 1
+}
 
 //他の場所で読み込んで使えるようにexport
 export default App;
